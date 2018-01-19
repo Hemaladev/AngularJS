d21c2c15a82de303382b5db50f3859901c4993ce
hemaladevanan125@gmail.com <Administrator@ITPRO-21.stu.com>
2018-01-19 10:34:35 +0530

    Angularjs simple example

---
+++ app.js
@@ -0,0 +1,179 @@
+(function(){
+    var app = angular.module("demo",[]);
+    app.controller("ContactController",function(){
+        this.contacts = contact;
+    });
+    var contact = [{
+        firstName : "Hemala",
+        lastNmae : "Devanan",
+        dob : "12/5/1997",
+        email : "hemala@gmail.com",
+        phoneNo : "9842560456",
+        canEdit : true,
+        deleteContact : false
+    },{
+        firstName : "Gayatri",
+        lastNmae : "Shankar",
+        dob : "19/8/1996",
+        email : "gayatri@gmail.com",
+        phoneNo : "9987654321",
+        canEdit : true,
+        deleteContact : false    
+    },{
+        firstName : "Dharini",
+        lastNmae : "s.k",
+        dob : "1/3/1998",
+        email : "dhari@gmail.com",
+        phoneNo : "934554321",
+        canEdit : true,
+        deleteContact : false
+    },{
+        firstName : "Hemala",
+        lastNmae : "Devanan",
+        dob : "12/5/1997",
+        email : "hemala@gmail.com",
+        phoneNo : "9842560456",
+        canEdit : true,
+        deleteContact : false
+        
+    },{
+        firstName : "Gayatri",
+        lastNmae : "Shankar",
+        dob : "19/8/1996",
+        email : "gayatri@gmail.com",
+        phoneNo : "9987654321",
+        canEdit : true,
+        deleteContact : false    
+    },{
+        firstName : "Dharini",
+        lastNmae : "s.k",
+        dob : "1/3/1998",
+        email : "dhari@gmail.com",
+        phoneNo : "934554321",
+        canEdit : true,
+        deleteContact : false
+    },{
+        firstName : "Hemala",
+        lastNmae : "Devanan",
+        dob : "12/5/1997",
+        email : "hemala@gmail.com",
+        phoneNo : "9842560456",
+        canEdit : true,
+        deleteContact : false
+        
+    },{
+        firstName : "Gayatri",
+        lastNmae : "Shankar",
+        dob : "19/8/1996",
+        email : "gayatri@gmail.com",
+        phoneNo : "9987654321"    
+    },{
+        firstName : "Dharini",
+        lastNmae : "s.k",
+        dob : "1/3/1998",
+        email : "dhari@gmail.com",
+        phoneNo : "934554321",
+        canEdit : true,
+        deleteContact : false
+    },{
+        firstName : "Hemala",
+        lastNmae : "Devanan",
+        dob : "12/5/1997",
+        email : "hemala@gmail.com",
+        phoneNo : "9842560456",
+        canEdit : true,
+        deleteContact : false
+        
+    },{
+        firstName : "Gayatri",
+        lastNmae : "Shankar",
+        dob : "19/8/1996",
+        email : "gayatri@gmail.com",
+        phoneNo : "9987654321" ,
+        canEdit : true,
+        deleteContact : false   
+    },{
+        firstName : "Dharini",
+        lastNmae : "s.k",
+        dob : "1/3/1998",
+        email : "dhari@gmail.com",
+        phoneNo : "934554321",
+        canEdit : true,
+        deleteContact : false
+    },{
+        firstName : "Hemala",
+        lastNmae : "Devanan",
+        dob : "12/5/1997",
+        email : "hemala@gmail.com",
+        phoneNo : "9842560456",
+        canEdit : true,
+        deleteContact : false
+        
+    },{
+        firstName : "Gayatri",
+        lastNmae : "Shankar",
+        dob : "19/8/1996",
+        email : "gayatri@gmail.com",
+        phoneNo : "9987654321" ,
+        canEdit : true,
+        deleteContact : false   
+    },{
+        firstName : "Dharini",
+        lastNmae : "s.k",
+        dob : "1/3/1998",
+        email : "dhari@gmail.com",
+        phoneNo : "934554321",
+        canEdit : true,
+        deleteContact : false
+    },{
+        firstName : "Hemala",
+        lastNmae : "Devanan",
+        dob : "12/5/1997",
+        email : "hemala@gmail.com",
+        phoneNo : "9842560456",
+        canEdit : true,
+        deleteContact : false
+        
+    },{
+        firstName : "Gayatri",
+        lastNmae : "Shankar",
+        dob : "19/8/1996",
+        email : "gayatri@gmail.com",
+        phoneNo : "9987654321",
+        canEdit : true,
+        deleteContact : false    
+    },{
+        firstName : "Dharini",
+        lastNmae : "s.k",
+        dob : "1/3/1998",
+        email : "dhari@gmail.com",
+        phoneNo : "934554321",
+        canEdit : true,
+        deleteContact : false
+    },{
+        firstName : "Hemala",
+        lastNmae : "Devanan",
+        dob : "12/5/1997",
+        email : "hemala@gmail.com",
+        phoneNo : "9842560456",
+        canEdit : true,
+        deleteContact : false
+        
+    },{
+        firstName : "Gayatri",
+        lastNmae : "Shankar",
+        dob : "19/8/1996",
+        email : "gayatri@gmail.com",
+        phoneNo : "9987654321" ,
+        canEdit : true,
+        deleteContact : false   
+    },{
+        firstName : "Dharini",
+        lastNmae : "s.k",
+        dob : "1/3/1998",
+        email : "dhari@gmail.com",
+        phoneNo : "934554321",
+        canEdit : true,
+        deleteContact : false
+    }];
+})();
