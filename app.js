770c53c92f0f58abf5422eadda344475f9c5fb5b
hemaladevanan125@gmail.com <Administrator@ITPRO-21.stu.com>
2018-01-19 10:39:15 +0530

    Table in AngularJS

--- app.js
+++ app.js
@@ -1,179 +1,37 @@
-(function(){
-    var app = angular.module("demo",[]);
-    app.controller("ContactController",function(){
-        this.contacts = contact;
+(function () {
+    var app = angular.module("demo", []);
+    app.controller("StoreController", function () {
+        this.contacts = phoneBook
     });
-    var contact = [{
-        firstName : "Hemala",
-        lastNmae : "Devanan",
-        dob : "12/5/1997",
-        email : "hemala@gmail.com",
-        phoneNo : "9842560456",
-        canEdit : true,
-        deleteContact : false
-    },{
-        firstName : "Gayatri",
-        lastNmae : "Shankar",
-        dob : "19/8/1996",
-        email : "gayatri@gmail.com",
-        phoneNo : "9987654321",
-        canEdit : true,
-        deleteContact : false    
-    },{
-        firstName : "Dharini",
-        lastNmae : "s.k",
-        dob : "1/3/1998",
-        email : "dhari@gmail.com",
-        phoneNo : "934554321",
-        canEdit : true,
-        deleteContact : false
-    },{
-        firstName : "Hemala",
-        lastNmae : "Devanan",
-        dob : "12/5/1997",
-        email : "hemala@gmail.com",
-        phoneNo : "9842560456",
-        canEdit : true,
-        deleteContact : false
-        
-    },{
-        firstName : "Gayatri",
-        lastNmae : "Shankar",
-        dob : "19/8/1996",
-        email : "gayatri@gmail.com",
-        phoneNo : "9987654321",
-        canEdit : true,
-        deleteContact : false    
-    },{
-        firstName : "Dharini",
-        lastNmae : "s.k",
-        dob : "1/3/1998",
-        email : "dhari@gmail.com",
-        phoneNo : "934554321",
-        canEdit : true,
-        deleteContact : false
-    },{
-        firstName : "Hemala",
-        lastNmae : "Devanan",
-        dob : "12/5/1997",
-        email : "hemala@gmail.com",
-        phoneNo : "9842560456",
-        canEdit : true,
-        deleteContact : false
-        
-    },{
-        firstName : "Gayatri",
-        lastNmae : "Shankar",
-        dob : "19/8/1996",
-        email : "gayatri@gmail.com",
-        phoneNo : "9987654321"    
-    },{
-        firstName : "Dharini",
-        lastNmae : "s.k",
-        dob : "1/3/1998",
-        email : "dhari@gmail.com",
-        phoneNo : "934554321",
-        canEdit : true,
-        deleteContact : false
-    },{
-        firstName : "Hemala",
-        lastNmae : "Devanan",
-        dob : "12/5/1997",
-        email : "hemala@gmail.com",
-        phoneNo : "9842560456",
-        canEdit : true,
-        deleteContact : false
-        
-    },{
-        firstName : "Gayatri",
-        lastNmae : "Shankar",
-        dob : "19/8/1996",
-        email : "gayatri@gmail.com",
-        phoneNo : "9987654321" ,
-        canEdit : true,
-        deleteContact : false   
-    },{
-        firstName : "Dharini",
-        lastNmae : "s.k",
-        dob : "1/3/1998",
-        email : "dhari@gmail.com",
-        phoneNo : "934554321",
-        canEdit : true,
-        deleteContact : false
-    },{
-        firstName : "Hemala",
-        lastNmae : "Devanan",
-        dob : "12/5/1997",
-        email : "hemala@gmail.com",
-        phoneNo : "9842560456",
-        canEdit : true,
-        deleteContact : false
-        
-    },{
-        firstName : "Gayatri",
-        lastNmae : "Shankar",
-        dob : "19/8/1996",
-        email : "gayatri@gmail.com",
-        phoneNo : "9987654321" ,
-        canEdit : true,
-        deleteContact : false   
-    },{
-        firstName : "Dharini",
-        lastNmae : "s.k",
-        dob : "1/3/1998",
-        email : "dhari@gmail.com",
-        phoneNo : "934554321",
-        canEdit : true,
-        deleteContact : false
-    },{
-        firstName : "Hemala",
-        lastNmae : "Devanan",
-        dob : "12/5/1997",
-        email : "hemala@gmail.com",
-        phoneNo : "9842560456",
-        canEdit : true,
-        deleteContact : false
-        
-    },{
-        firstName : "Gayatri",
-        lastNmae : "Shankar",
-        dob : "19/8/1996",
-        email : "gayatri@gmail.com",
-        phoneNo : "9987654321",
-        canEdit : true,
-        deleteContact : false    
-    },{
-        firstName : "Dharini",
-        lastNmae : "s.k",
-        dob : "1/3/1998",
-        email : "dhari@gmail.com",
-        phoneNo : "934554321",
-        canEdit : true,
-        deleteContact : false
-    },{
-        firstName : "Hemala",
-        lastNmae : "Devanan",
-        dob : "12/5/1997",
-        email : "hemala@gmail.com",
-        phoneNo : "9842560456",
-        canEdit : true,
-        deleteContact : false
-        
-    },{
-        firstName : "Gayatri",
-        lastNmae : "Shankar",
-        dob : "19/8/1996",
-        email : "gayatri@gmail.com",
-        phoneNo : "9987654321" ,
-        canEdit : true,
-        deleteContact : false   
-    },{
-        firstName : "Dharini",
-        lastNmae : "s.k",
-        dob : "1/3/1998",
-        email : "dhari@gmail.com",
-        phoneNo : "934554321",
-        canEdit : true,
-        deleteContact : false
+    var phoneBook = [{
+        firstName: "Steve",
+        lastName: "Jobs",
+        email: "steve@apple.com",
+        phone: "+91-9876543210"
+    }, {
+        firstName: "Steve",
+        lastName: "Jobs",
+        email: "steve@apple.com",
+        phone: "+91-9876543210"
+    }, {
+        firstName: "Steve",
+        lastName: "Jobs",
+        email: "steve@apple.com",
+        phone: "+91-9876543210"
+    }, {
+        firstName: "Steve",
+        lastName: "Jobs",
+        email: "steve@apple.com",
+        phone: "+91-9876543210"
+    }, {
+        firstName: "Steve",
+        lastName: "Jobs",
+        email: "steve@apple.com",
+        phone: "+91-9876543210"
+    }, {
+        firstName: "Steve",
+        lastName: "Jobs",
+        email: "steve@apple.com",
+        phone: "+91-9876543210"
     }];
 })();
