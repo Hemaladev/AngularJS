8add77b6cd8b7146b690a3a0a1fadf1541f1a006
hemaladevanan125@gmail.com <Administrator@ITPRO-21.stu.com>
2018-01-19 11:53:29 +0530

    Deleting a row in a table by clicking delete button

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 (function () {
     var app = angular.module("demo", []);
-    app.controller("StoreController", function () {
-        this.contact = phoneBook;
-        var pb = this.contact;
-        this.showContact = function() {
-            console.log(pb);
-            //send ajax request here
+    app.controller("ContactController", function ($http) {
+        var pb = this;
+        this.contacts = [];
+        this.delete = function(loc) {
+            pb.contacts.splice(loc, 1)
         };
+        
+        $http.get("phone-book.json").then(function(res) {
+            console.log(res)
+            pb.contacts = res.data.contacts
+        },function(res) {
+            console.log("Error")
+        })
     });
-    var phoneBook = {
-        firstName: "Steve",
-        lastName: "Jobs",
-        email: "steve@apple.com",
-        phone: "+91-9876543210",
-        gender: "Male"
-    };
 })();
-
